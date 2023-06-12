36a8884fed0176a4aecd05af5962ddba002fd00b
network_crab <network123crab@gmail.com>
2023-06-12 08:39:23 +0530

    odd_even_concept

---
+++ even_odd.php
@@ -0,0 +1,13 @@
+<?php
+
+# Taking the number as input
+$num = readline("Enter the number: ");
+
+# Building the logic
+if($num % 2 == 0){
+	echo "The number you entered is even number !";
+} else {
+	echo "The number you entered is odd number !";
+}
+
+?>
