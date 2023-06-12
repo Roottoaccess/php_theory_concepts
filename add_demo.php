894a6565ad0da929a984898ed3e95bb036fe3f11
network_crab <network123crab@gmail.com>
2023-06-12 20:16:14 +0530

    all for myself

---
+++ add_demo.php
@@ -0,0 +1,8 @@
+<?php
+$result = 0;
+for($i = 0; $i <= 30; $i++){
+    $result += $i;
+}
+echo "The sum is: $result";
+
+?>
---
+++ data_types.php
@@ -0,0 +1,28 @@
+<?php
+# There are 8 data types in php and its merged in 3 types
+# Scaller type
+# Compound type
+# Special type
+
+# Scaller Type
+$intt = 25;
+echo "This $intt is an integer datatype.";
+
+$flott = 24.88;
+echo "This $flott is an float datatype.";
+
+$strr = "Hello World";
+echo "This '$strr' is a string datatype.";
+
+if(True){
+	echo "This is TRUE";
+} if(False) { 
+	echo "This is not TRUE";
+}
+
+# Compound type
+
+$arr = array(1,2,3,4,5,56);
+
+print("The first element of the array is: ".$arr[0]);
+?>
